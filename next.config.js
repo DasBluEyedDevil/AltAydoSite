de96ddddcfd9b2eb00d5d4bda3f2743bcc487967
Devil <dasblueeyeddevil@gmail.com>
2025-05-22 20:57:42 -0400

    Enhance image configuration in next.config.js
    - Added image settings to support SVGs and specify formats for optimized loading.
    - Configured content security policy for improved security measures.

--- next.config.js
+++ next.config.js
@@ -10,6 +10,13 @@ const nextConfig = {
         // Dangerously allow production builds to successfully complete even if
         // your project has type errors.
         ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: false,
+        dangerouslyAllowSVG: true,
+        domains: [],
+        formats: ['image/avif', 'image/webp'],
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     }
 };
 
